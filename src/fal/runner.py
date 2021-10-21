05c5bf98027a7e440116e4a93ee2d6ba9bf98bc7
Meder <meder@kamalov.net>
2021-10-21 21:51:24 +0200

    Update project

--- src/fal/runner.py
+++ src/fal/runner.py
@@ -1,20 +1,29 @@
 """Run forecasts with FAL."""
 import click
+import os
 import yaml
 import json
 import pandas as pd
 import datetime
+import time
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 from google.cloud import bigquery, bigquery_storage
 
 
 DBT_PROJECT_YML = "dbt_project.yml"
 DBT_MANIFEST_JSON = "manifest.json"
+FORECAST_PREFIX = "fal_forecast_"
+CHANNEL_ID = "C01GYF1KWV7"
+SLACK_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 
 
 @click.command()
 @click.argument("model")
-def run(model: str):
+@click.argument("message")
+def run(model: str, message: str):
     """Run forecast."""
     try:
         with open(DBT_PROJECT_YML) as raw_project:
@@ -23,8 +32,15 @@ def run(model: str):
             model_table = find_model_table(
                 model_name=model, target_path=target_path)
             df = get_bq_df(table_id=model_table)
-            forecast = make_forecast(dataframe=df)
-            print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+            forecast = make_forecast(
+                dataframe=df,
+                filename=f"{FORECAST_PREFIX}{time.time()}.png")
+            send_slack_file(
+                file_path=forecast,
+                message_text=message,
+                channel_id=CHANNEL_ID,
+                slack_token=SLACK_TOKEN
+            )
     except (OSError, yaml.YAMLError):
         raise OSError(
             f"File {DBT_PROJECT_YML} not found")
@@ -51,7 +67,7 @@ def get_bq_df(table_id: str):
     return rows.to_dataframe(bqstorage_client=bq_storage_client)
 
 
-def make_forecast(dataframe: pd.DataFrame):
+def make_forecast(dataframe: pd.DataFrame, filename: str):
     """Make forecast on monthly data."""
     m = Prophet(daily_seasonality=False, weekly_seasonality=False)
     m.fit(dataframe)
@@ -68,4 +84,26 @@ def make_forecast(dataframe: pd.DataFrame):
             year += 1
         future_dates.append(datetime.datetime(year=year, month=month, day=1))
     df_future = pd.DataFrame({'ds': future_dates})
-    return m.predict(df_future)
+    forecast = m.predict(df_future)
+    fig = plot_plotly(m, forecast, xlabel='Month', ylabel='Ozone (ppm)')
+    fig.write_image(filename)
+    return filename
+
+
+def send_slack_file(
+        file_path: str,
+        message_text: str,
+        channel_id: str,
+        slack_token: str):
+    """Send file to slack."""
+    client = WebClient(token=slack_token)
+
+    try:
+        client.files_upload(
+            channels=channel_id,
+            file=file_path,
+            title="FAL forecast",
+            initial_comment=message_text
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
