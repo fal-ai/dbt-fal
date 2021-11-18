37f37664c179fde79ae68431160046994a3f8cf5
turbo1912 <yurtseven.gorkem@gmail.com>
2021-11-17 17:17:04 -0800

    Run tests on PR

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,12 +6,12 @@ from pathlib import Path
 import shutil
 import pytest
 
+profiles_dir = os.path.join(Path.cwd(), "tests/mock/mockProfile")
 
 def test_run():
     runner = CliRunner()
-    result = runner.invoke(cli, ["run"])
+    result = runner.invoke(cli, ["run", "--profiles-dir", profiles_dir])
     assert "no dbt_project.yml found at expected path" in str(result.exception)
-
 
 def test_no_arg():
     runner = CliRunner()
@@ -19,15 +19,13 @@ def test_no_arg():
     assert result.exit_code == 0
     assert "Usage: cli [OPTIONS] COMMAND [ARGS]..." in result.output
 
-
-@pytest.mark.skip("Getting 'Could not find profile named 'fal_dbt_examples''")
 def test_run_with_project_dir():
     runner = CliRunner()
     with tempfile.TemporaryDirectory() as tmp_dir:
         shutil.copytree(
             os.path.join(Path.cwd(), "tests/mock"), tmp_dir, dirs_exist_ok=True
         )
-        result = runner.invoke(cli, ["run", "--project-dir", tmp_dir])
+        result = runner.invoke(cli, ["run", "--project-dir", tmp_dir, "--profiles-dir", profiles_dir])
     assert result.exit_code == 0
 
 
