4135832cbab8e1d0f530bbf2d7056e2b03349155
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-01-13 19:05:48 -0400

    Add tests for run_results not present

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,7 @@ import shutil
 import pytest
 
 profiles_dir = os.path.join(Path.cwd(), "tests/mock/mockProfile")
+project_dir = os.path.join(Path.cwd(), "tests/mock")
 
 
 def test_run():
@@ -25,9 +26,7 @@ def test_no_arg():
 def test_run_with_project_dir():
     runner = CliRunner()
     with tempfile.TemporaryDirectory() as tmp_dir:
-        shutil.copytree(
-            os.path.join(Path.cwd(), "tests/mock"), tmp_dir, dirs_exist_ok=True
-        )
+        shutil.copytree(project_dir, tmp_dir, dirs_exist_ok=True)
         result = runner.invoke(
             cli, ["run", "--project-dir", tmp_dir, "--profiles-dir", profiles_dir]
         )
@@ -47,9 +46,7 @@ def test_version():
 def test_selection(capfd):
     runner = CliRunner()
     with tempfile.TemporaryDirectory() as tmp_dir:
-        shutil.copytree(
-            os.path.join(Path.cwd(), "tests/mock"), tmp_dir, dirs_exist_ok=True
-        )
+        shutil.copytree(project_dir, tmp_dir, dirs_exist_ok=True)
 
         result = runner.invoke(
             cli,
@@ -91,3 +88,30 @@ def test_selection(capfd):
         assert "model_empty_scripts" not in captured.out
         # It has no keyword in meta
         assert "model_no_fal" not in captured.out
+
+
+def test_no_run_results(capfd):
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        shutil.copytree(project_dir, tmp_dir, dirs_exist_ok=True)
+        shutil.rmtree(os.path.join(tmp_dir, "mockTarget"))
+
+        # Without selection flag
+        result = runner.invoke(
+            cli, ["run", "--project-dir", tmp_dir, "--profiles-dir", profiles_dir]
+        )
+        assert result.exit_code == 1
+        assert (
+            "Cannot define models to run without selection flags or dbt run_results artifact"
+            in str(result.exception)
+        )
+
+        # With selection flag
+        result = runner.invoke(
+            cli,
+            ["run", "--project-dir", tmp_dir, "--profiles-dir", profiles_dir, "--all"],
+        )
+        captured = capfd.readouterr()
+        assert result.exit_code == 0
+        # Just as warning
+        assert "Could not read dbt run_results artifact" in captured.out
