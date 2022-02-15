881327ec04cfa2c1cea2f58e73db7b0247bff096
Meder Kamalov <38204337+mkam039@users.noreply.github.com>
2022-02-15 19:47:44 +0100

    Use argparse (#125)
    * Refactor to use argparse
    
    * Handle --version flag
    
    * Update cli unit tests
    
    * Remove click dependency
    
    * Use sub_parsers instead of class methods

--- src/fal/cli.py
+++ src/fal/cli.py
@@ -1,5 +1,7 @@
-import click
+import argparse
 import os
+import sys
+import pkg_resources
 
 from dbt.logger import log_manager
 from dbt.config.profile import DEFAULT_PROFILES_DIR
@@ -10,93 +12,102 @@ from fal.utils import print_run_info
 from faldbt.project import FalDbt, FalGeneralException, FalProject
 
 
-@click.group()
-@click.version_option()
+def run_fal(argv):
+
+    parser = argparse.ArgumentParser(
+        description="Run Python scripts on dbt models",
+        usage='''fal COMMAND [<args>]''',
+        prog='fal')
+
+    if len(argv) < 2:
+        print("No command supplied\n")
+        parser.print_help()
+        exit(1)
+
+    # Handle version checking
+    version = pkg_resources.get_distribution('fal').version
+    parser.add_argument("-v",
+                        "--version",
+                        action="version",
+                        version=f"fal {version}",
+                        help="show fal version")
+
+    # Handle commands
+    command_parsers = parser.add_subparsers(title="commands",
+                                            dest="command_parsers")
+    run_parser = command_parsers.add_parser(
+        'run',
+        help='Run Python scripts as final nodes'
+    )
+
+    run_parser.add_argument("--project-dir",
+                            default=os.getcwd(),
+                            help="Directory to look for dbt_project.yml.")
+    run_parser.add_argument("--profiles-dir",
+                            default=None,
+                            help="Directory to look for profiles.yml.")
+    run_parser.add_argument("--keyword",
+                            default="fal",
+                            help="Property in meta to look for fal configurations.")
+    run_parser.add_argument("--all",
+                            default=False,
+                            action='store_true',
+                            help="Run scripts for all models. By default, fal runs scripts for models that ran in the last dbt run.")
+    run_parser.add_argument("-s", "--select",
+                            default=tuple(),
+                            nargs="+",
+                            help="Specify the nodes to include.")
+    run_parser.add_argument("-m", "--models",
+                            default=tuple(),
+                            nargs="+",
+                            help="Specify the nodes to include.")
+    run_parser.add_argument("--exclude",
+                            default=tuple(),
+                            nargs="+",
+                            help="Specify the nodes to exclude.")
+    run_parser.add_argument("--selector",
+                            default=None,
+                            action='store_true',
+                            help="The selector name to use, as defined in selectors.yml",)
+    run_parser.add_argument("--scripts",
+                            default=None,
+                            nargs="+",
+                            help="Specify scripts to run, overrides schema.yml",)
+    run_parser.add_argument("--before",
+                            action='store_true',
+                            help="Run scripts specified in model `before` tag",
+                            default=False)
+    run_parser.add_argument("--experimental-ordering",
+                            action='store_true',
+                            help="Turns on ordering of the fal scripts.",
+                            default=False)
+    run_parser.add_argument("--debug",
+                            action='store_true',
+                            help="Display debug logging during execution.",
+                            default=False)
+
+    args = parser.parse_args(argv[1:])
+
+    _run(
+        project_dir=args.project_dir,
+        profiles_dir=args.profiles_dir,
+        keyword=args.keyword,
+        all=args.all,
+        select=args.select,
+        models=args.models,
+        exclude=args.exclude,
+        selector=args.selector,
+        script=args.scripts,
+        before=args.before,
+        experimental_ordering=args.experimental_ordering,
+        debug=args.debug)
+
+
 def cli():
-    pass
+    run_fal(sys.argv)
 
 
-@cli.command()
-@click.option(
-    "--project-dir",
-    default=os.getcwd(),
-    help="Directory to look for dbt_project.yml.",
-    type=click.Path(exists=True),
-)
-@click.option(
-    "--profiles-dir",
-    default=None,
-    help="Directory to look for profiles.yml.",
-    type=click.Path(exists=True),
-)
-@click.option(
-    "--keyword",
-    default="fal",
-    help="Property in meta to look for fal configurations.",
-    type=click.STRING,
-)
-@click.option(
-    "--all",
-    help="Run scripts for all models. By default, fal runs scripts for models that ran in the last dbt run.",
-    is_flag=True,
-)
-@click.option(
-    "--select",
-    "-s",
-    multiple=True,
-    default=tuple(),
-    nargs=1,
-    help="Specify the nodes to include.",
-    type=click.STRING,
-)
-@click.option(
-    "--models",
-    "-m",
-    nargs=1,
-    multiple=True,
-    default=tuple(),
-    help="Specify the nodes to include.",
-    type=click.STRING,
-)
-@click.option(
-    "--exclude",
-    nargs=1,
-    multiple=True,
-    default=tuple(),
-    help="Specify the models to exclude.",
-    type=click.STRING,
-)
-@click.option(
-    "--selector",
-    nargs=1,
-    default=None,
-    help="The selector name to use, as defined in selectors.yml",
-    type=click.STRING,
-)
-@click.option(
-    "--script",
-    nargs=1,
-    default=None,
-    multiple=True,
-    help="Specify scripts to run, overrides schema.yml",
-    type=click.STRING,
-)
-@click.option(
-    "--before",
-    help="Run scripts specified in model `before` tag",
-    is_flag=True
-)
-@click.option(
-    "--experimental-ordering",
-    help="Turns on ordering of the fal scripts.",
-    is_flag=True,
-)
-@click.option(
-    "--debug",
-    help="Display debug logging during execution.",
-    is_flag=True,
-)
-def run(
+def _run(
     project_dir,
     profiles_dir,
     keyword,
