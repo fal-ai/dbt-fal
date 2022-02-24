8f17b4f32c96f8d3e2fbb1534ccebca3a9276bcf
gorkem <yurtseven.gorkem@gmail.com>
2022-02-23 23:52:55 -0800

    Remove ScriptDag in lieu of the more generic NodeDag (#158)

---
+++ src/fal/fal_script.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass, field
+from typing import List, TypeVar, Dict, Union
+from faldbt.project import DbtModel, FalDbt
+from pathlib import Path
+
+T = TypeVar("T", bound="FalScript")
+
+
+class FalDagCycle(Exception):
+    pass
+
+
+@dataclass(frozen=True)
+class FalScript:
+    model: Union[DbtModel, None]
+    path: Path
+
+    def exec(self, context, faldbt: FalDbt):
+        """
+        Executes the script
+        """
+        with open(self.path) as file:
+            a_script = file.read()
+            exec(
+                a_script,
+                {
+                    "context": context,
+                    "ref": faldbt.ref,
+                    "source": faldbt.source,
+                    "write_to_source": faldbt.write_to_source,
+                    "write_to_firestore": faldbt.write_to_firestore,
+                    "list_models": faldbt.list_models,
+                    "list_models_ids": faldbt.list_models_ids,
+                    "list_sources": faldbt.list_sources,
+                    "list_features": faldbt.list_features,
+                },
+            )
