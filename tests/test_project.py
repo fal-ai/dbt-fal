37c2c90c1bb47febf822a0d3f98e10dfe6973cbf
Meder <meder@kamalov.net>
2022-03-11 10:56:13 +0100

    Add tests

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from mock import Mock
 
 from fal import FalDbt
 
@@ -14,7 +15,7 @@ def test_scripts():
     )
 
     assert isinstance(faldbt._global_script_paths, dict)
-    assert 0 == len(faldbt._global_script_paths['after'])
+    assert 0 == len(faldbt._global_script_paths["after"])
 
     faldbt._global_script_paths
     models = faldbt.list_models()
@@ -44,3 +45,115 @@ def test_features():
     fs_def = features[0]
     assert "a" == fs_def.entity_column
     assert "model_feature_store" == fs_def.model
+
+
+def test_adapter_setup():
+    faldbt = FalDbt(
+        profiles_dir=profiles_dir,
+        project_dir=project_dir,
+    )
+
+    fivetran_client = faldbt._el_configs["fivetran"].get("client", None)
+    assert fivetran_client is not None
+    assert fivetran_client.api_key == "my_fivetran_key"
+    assert fivetran_client.api_secret == "my_fivetran_secret"
+
+    connectors = faldbt._el_configs["fivetran"].get("connectors", None)
+    assert len(connectors) == 2
+    assert connectors[0]["name"] is not None
+    assert connectors[0]["id"] is not None
+
+    airbyte_client = faldbt._el_configs["airbyte"].get("client", None)
+    assert airbyte_client is not None
+    assert airbyte_client.host == "http://localhost:8001"
+
+    connections = faldbt._el_configs["airbyte"].get("connections", None)
+    assert len(connections) == 2
+    assert connections[0]["name"] is not None
+    assert connections[0]["id"] is not None
+
+
+def test_airbyte_sync():
+    faldbt = FalDbt(
+        profiles_dir=profiles_dir,
+        project_dir=project_dir,
+    )
+    airbyte_client = faldbt._el_configs["airbyte"].get("client", None)
+    airbyte_client.sync_and_wait = Mock()
+
+    faldbt.airbyte_sync(connection_id="id_airbyte_connection_1")
+    airbyte_client.sync_and_wait.assert_called_with(
+        "id_airbyte_connection_1", interval=10, timeout=None
+    )
+
+    airbyte_client.sync_and_wait.reset_mock()
+
+    faldbt.airbyte_sync(connection_name="airbyte_connection_1")
+    airbyte_client.sync_and_wait.assert_called_with(
+        "id_airbyte_connection_1", interval=10, timeout=None
+    )
+
+    airbyte_client.sync_and_wait.reset_mock()
+
+    try:
+        faldbt.airbyte_sync()
+
+    except Exception as e:
+        assert str(e) == "Either connection id or connection name have to be provided."
+
+    airbyte_client.sync_and_wait.reset_mock()
+
+    try:
+        faldbt.airbyte_sync(connection_name="not_there")
+        assert False is True
+    except Exception as e:
+        assert (
+            str(e)
+            == "Couldn't find connection not_there. Did you add it to profiles.yml?"
+        )
+
+
+def test_fivetran_sync():
+    faldbt = FalDbt(
+        profiles_dir=profiles_dir,
+        project_dir=project_dir,
+    )
+    fivetran_client = faldbt._el_configs["fivetran"].get("client", None)
+    fivetran_client.sync_and_wait = Mock()
+    fivetran_client.resync_and_wait = Mock()
+
+    faldbt.fivetran_sync(connector_id="id_fivetran_connector_1")
+    fivetran_client.sync_and_wait.assert_called_with(
+        "id_fivetran_connector_1", interval=10, timeout=None
+    )
+
+    fivetran_client.sync_and_wait.reset_mock()
+
+    faldbt.fivetran_sync(connector_name="fivetran_connector_1")
+    fivetran_client.sync_and_wait.assert_called_with(
+        "id_fivetran_connector_1", interval=10, timeout=None
+    )
+
+    fivetran_client.sync_and_wait.reset_mock()
+
+    try:
+        faldbt.fivetran_sync()
+
+    except Exception as e:
+        assert str(e) == "Either connector id or connector name have to be provided."
+
+    fivetran_client.sync_and_wait.reset_mock()
+
+    try:
+        faldbt.fivetran_sync(connector_name="not_there")
+        assert False is True
+    except Exception as e:
+        assert (
+            str(e)
+            == "Couldn't find connector not_there. Did you add it to profiles.yml?"
+        )
+
+    faldbt.fivetran_sync(connector_name="fivetran_connector_1", historical=True)
+    fivetran_client.resync_and_wait.assert_called_with(
+        "id_fivetran_connector_1", interval=10, timeout=None
+    )
