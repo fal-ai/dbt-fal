ac09f9ab06c4bc14db7c61029420b6429f6a2c4a
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-03-25 18:31:05 -0400

    New tests with complex imports

--- integration_tests/mock/fal_scripts/test.py
+++ integration_tests/mock/fal_scripts/test.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import io
-import my_util
+from utils.my_util import handle_dbt_test, model_info_str
 
+model_info = model_info_str(context.current_model)
 model_name = context.current_model.name
-
 output = f"Model name: {model_name}"
 
 if context.current_model.status == "tested":
-    my_util.handle_dbt_test(context.current_model.tests, output, model_name)
+    handle_dbt_test(context.current_model.tests, output, model_name)
 
 else:
     output = f"Model name: {model_name}"
@@ -20,11 +20,10 @@ else:
 
     output = output + f"\nModel dataframe information:\n{info}"
 
-    f = open(f"temp/{model_name}", "w")
-    f.write(output)
-    f.close()
+    with open(f"temp/{model_name}", "w") as f:
+        f.write(output)
 
-    # Pass information to outside of this script
+    # TODO: Pass information to outside of this script
     RESULT = df.size
 
-print(f"Test done for {model_name}")
+print(f"Script test.py done for {model_info}")
