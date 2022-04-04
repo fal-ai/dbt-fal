5148f58c63421e987536c13d8c306e2525508ceb
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-04-04 15:25:48 -0400

    Remove ref to non-yet-existent relation in integration test (#226)

--- integration_tests/projects/002_jaffle_shop/load_data.py
+++ integration_tests/projects/002_jaffle_shop/load_data.py
@@ -8,12 +8,6 @@ model_name = context.current_model.name
 output = f"Model name: {model_name}"
 output = output + f"\nStatus: {context.current_model.status}"
 
-df: pd.DataFrame = ref(model_name)
-buf = io.StringIO()
-df.info(buf=buf, memory_usage=False)
-info = buf.getvalue()
-
-output = output + f"\nModel dataframe information:\n{info}"
 temp_dir = os.environ["temp_dir"]
 write_dir = open(reduce(os.path.join, [temp_dir, model_name + ".load_data.py"]), "w")
 write_dir.write(output)
