d5a6a365412c9f14faa99f18f18d94aceb275ba2
Meder <meder@kamalov.net>
2022-04-05 21:36:38 +0200

    PR comments

--- integration_tests/projects/004_script_paths/scripts/before.py
+++ integration_tests/projects/004_script_paths/scripts/before.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import io
 import os
 from functools import reduce
 
@@ -7,15 +5,8 @@ model_name = context.current_model.name
 
 output = f"Model name: {model_name}"
 output = output + f"\nStatus: {context.current_model.status}"
-
-df: pd.DataFrame = ref(model_name)
-buf = io.StringIO()
-df.info(buf=buf, memory_usage=False)
-info = buf.getvalue()
-
-output = output + f"\nModel dataframe information:\n{info}"
 temp_dir = os.environ["temp_dir"]
-print(temp_dir)
+
 write_dir = open(reduce(os.path.join, [temp_dir, model_name + ".before.py"]), "w")
 write_dir.write(output)
 write_dir.close()
