dd07dc60eb5db1b4e6b844510198f8398bf33483
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-04-21 10:12:17 -0400

    Make casting work for all profiles

--- integration_tests/projects/005_functions_and_variables/models/other_model.sql
+++ integration_tests/projects/005_functions_and_variables/models/other_model.sql
@@ -2,10 +2,10 @@
 
 WITH data AS (
 
-    SELECT 
-        'some text'::text AS my_text, 
+    SELECT
+        'some text' AS my_text,
         -- The following column will be filled in an after script with fal
-        NULL::numeric AS my_float
+        cast(NULL AS numeric) AS my_float
 )
 
 SELECT *
