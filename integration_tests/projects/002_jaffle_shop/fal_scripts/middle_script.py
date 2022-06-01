2638c0d088d643c4d2b8126e0a19f3cf71f34a38
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-06-01 13:05:50 -0400

    feat: Set __name__ as __main__ for scripts ran

--- integration_tests/projects/002_jaffle_shop/fal_scripts/middle_script.py
+++ integration_tests/projects/002_jaffle_shop/fal_scripts/middle_script.py
@@ -1,10 +1,16 @@
 import os
 from functools import reduce
+from main_check_2 import main_check
 
 model_name = context.current_model.name
 
 output = f"Model name: {model_name}"
-output = output + f"\nStatus: {context.current_model.status}"
+output = output + f"\nStatus: {context.current_model.status}\n"
+
+output += f"top name: {__name__}\n"
+output = main_check(output)
+if __name__ == "__main__":
+    output += "passed main if\n"
 
 temp_dir = os.environ["temp_dir"]
 print(temp_dir)
