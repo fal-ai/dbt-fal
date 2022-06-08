e22db906b5ab30e4692ae37402467523d77e94c0
Batuhan Taskaya <isidentical@gmail.com>
2022-06-08 12:15:51 +0300

    tests: add support for testing pure-python models

--- integration_tests/projects/008_pure_python_models/models/staging/model_c.py
+++ integration_tests/projects/008_pure_python_models/models/staging/model_c.py
@@ -2,9 +2,24 @@
 DEPENDENCY: ref("model_b")
 - ref('model_a')
 """
+import os
+from functools import reduce
+
 # weird way to call, but added in the docstring
 df = ref(*["model_b"])
 
 df["my_bool"] = True
 
 write_to_model(df)
+
+model_name = context.current_model.name
+
+output = f"Model name: {model_name}"
+output = output + f"\nStatus: {context.current_model.status}"
+
+output = output + "\nModel dataframe name: {model_name}"
+temp_dir = os.environ["temp_dir"]
+
+write_dir = open(reduce(os.path.join, [temp_dir, model_name + ".txt"]), "w")
+write_dir.write(output)
+write_dir.close()
