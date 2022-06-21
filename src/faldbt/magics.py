59a8476082a7afcbcd83b6ad8a675558cc6a056a
Meder <meder@kamalov.net>
2022-06-21 09:29:40 +0200

    Add execute_sql to fal magics

--- src/faldbt/magics.py
+++ src/faldbt/magics.py
@@ -36,6 +36,7 @@ def init_fal(line="", local_ns={}):
         "list_models_ids": faldbt.list_models_ids,
         "list_sources": faldbt.list_sources,
         "list_features": faldbt.list_features,
+        "execute_sql": faldbt.execute_sql,
         "el": faldbt.el,
     }
 
