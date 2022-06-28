bb3ded1fddff3d5eb0a3917430b8217710e9f4ae
Batuhan Taskaya <isidentical@gmail.com>
2022-06-28 13:46:24 +0300

    tests: add a little delay to the post-hook script (#424)

--- integration_tests/projects/008_pure_python_models/scripts/post_hook.py
+++ integration_tests/projects/008_pure_python_models/scripts/post_hook.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from functools import reduce
 import os
+import time
+
+# Delay just a little bit in order to make sure the file is created
+# just after the model file.
+time.sleep(0.05)
+
 
 model_name = context.current_model.name
 
