8a67b84da4abc7d9fe68b01d463a114b76b9377e
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-07-05 12:51:29 -0400

    Add a threading lock for interactions that involve the cache

--- src/faldbt/lib.py
+++ src/faldbt/lib.py
@@ -6,11 +6,14 @@ from dataclasses import dataclass
 from uuid import uuid4
 from typing import Iterator, List, Optional, Tuple
 from urllib.parse import quote_plus
+import threading
 
 import dbt.version
 import dbt.semver
 import dbt.flags as flags
 import dbt.adapters.factory as adapters_factory
+from dbt.logger import GLOBAL_LOGGER as logger
+
 from dbt.contracts.connection import AdapterResponse
 from dbt.adapters.sql import SQLAdapter
 from dbt.adapters.base import BaseRelation, BaseAdapter, BaseConnectionManager
@@ -101,6 +104,33 @@ def _get_adapter(
     return adapter
 
 
+global _lock
+# RLock supports recursive locking by the same thread
+_lock = threading.RLock()
+
+
+@contextmanager
+def _cache_lock(info: str = ""):
+    operationId = uuid4()
+    logger.debug("Locking  {} {}", operationId, info)
+
+    _exc = None
+    _lock.acquire()
+    logger.debug("Acquired {}", operationId)
+
+    try:
+        yield
+    except BaseException as exc:
+        _exc = exc
+
+    _lock.release()
+    logger.debug("Released {}", operationId)
+    if _exc:
+        raise RuntimeError(
+            f"Error during cache lock (operation: {operationId})"
+        ) from _exc
+
+
 def _connection_name(prefix: str, obj, _hash: bool = True):
     # HACK: we need to include uniqueness (UUID4) to avoid clashes
     return f"{prefix}:{hash(str(obj)) if _hash else obj}:{uuid4()}"
@@ -136,7 +166,7 @@ def _execute_sql(
     return response, _agate_table_to_df(agate_table)
 
 
-def _clear_relations_cache(adapter: SQLAdapter, config: RuntimeConfig):
+def _clear_relations_cache(adapter: BaseAdapter, config: RuntimeConfig):
     # HACK: Sometimes we cache an incomplete cache or create stuff without the cache noticing.
     #       Some adapters work without this. We should separate adapter solutions like dbt.
     manifest = parse.get_dbt_manifest(config)
@@ -158,13 +188,15 @@ def _get_target_relation(
 
     relation = None
     with adapter.connection_named(_connection_name("relation", target)):
-        _clear_relations_cache(adapter, config)
-        target_name = target.name
-        if isinstance(target, ParsedModelNode):
-            target_name = target.alias
+        with _cache_lock("_get_target_relation"):
+            _clear_relations_cache(adapter, config)
 
-        # This ROLLBACKs so it has to be a new connection
-        relation = adapter.get_relation(target.database, target.schema, target_name)
+            target_name = target.name
+            if isinstance(target, ParsedModelNode):
+                target_name = target.alias
+
+            # This ROLLBACKs so it has to be a new connection
+            relation = adapter.get_relation(target.database, target.schema, target_name)
 
     return relation
 
@@ -334,7 +366,10 @@ def overwrite_target(
         return results
     except:
         _drop_relation(
-            project_dir, profiles_dir, temporal_relation, profile_target=profile_target
+            project_dir,
+            profiles_dir,
+            temporal_relation,
+            profile_target=profile_target,
         )
         raise
 
@@ -445,60 +480,61 @@ def _replace_relation(
     new_relation: BaseRelation,
 ):
     adapter = _get_adapter(project_dir, profiles_dir, profile_target)
+    config = parse.get_dbt_config(
+        project_dir=project_dir,
+        profiles_dir=profiles_dir,
+        profile_target=profile_target,
+    )
 
     with adapter.connection_named(
-        _connection_name("replace_relation", original_relation)
+        _connection_name("replace_relation", original_relation, _hash=False)
     ):
-        adapter.connections.begin()
+        with _cache_lock("_replace_relation"):
+            adapter.connections.begin()
 
-        if adapter.type() not in ("bigquery", "snowflake"):
-            # This is a 'DROP ... IF EXISTS', so it always works
-            adapter.drop_relation(original_relation)
+            _clear_relations_cache(adapter, config)
 
-        if adapter.type() == "athena":
-            # HACK: athena doesn't support renaming tables, we do it manually
-            create_stmt = f"create table {original_relation} as select * from {new_relation} with data"
-            _execute_sql(
-                project_dir,
-                profiles_dir,
-                six.text_type(create_stmt).strip(),
-                profile_target=profile_target,
-                adapter=adapter,
-            )
-            adapter.drop_relation(new_relation)
-        elif adapter.type() == "bigquery":
-            create_stmt = f"create or replace table {original_relation} as select * from {new_relation}"
-            _execute_sql(
-                project_dir,
-                profiles_dir,
-                six.text_type(create_stmt).strip(),
-                profile_target=profile_target,
-                adapter=adapter,
-            )
-            adapter.drop_relation(new_relation)
-        elif adapter.type() == "snowflake":
-            create_stmt = (
-                f"create or replace table {original_relation} clone {new_relation}"
-            )
-            _execute_sql(
-                project_dir,
-                profiles_dir,
-                six.text_type(create_stmt).strip(),
-                profile_target=profile_target,
-                adapter=adapter,
-            )
-            adapter.drop_relation(new_relation)
-        else:
-            adapter.rename_relation(new_relation, original_relation)
+            if adapter.type() not in ("bigquery", "snowflake"):
+                # This is a 'DROP ... IF EXISTS', so it always works
+                adapter.drop_relation(original_relation)
 
-        adapter.commit_if_has_connection()
+            if adapter.type() == "athena":
+                # HACK: athena doesn't support renaming tables, we do it manually
+                create_stmt = f"create table {original_relation} as select * from {new_relation} with data"
+                _execute_sql(
+                    project_dir,
+                    profiles_dir,
+                    six.text_type(create_stmt).strip(),
+                    profile_target=profile_target,
+                    adapter=adapter,
+                )
+                adapter.drop_relation(new_relation)
+            elif adapter.type() == "bigquery":
+                create_stmt = f"create or replace table {original_relation} as select * from {new_relation}"
+                _execute_sql(
+                    project_dir,
+                    profiles_dir,
+                    six.text_type(create_stmt).strip(),
+                    profile_target=profile_target,
+                    adapter=adapter,
+                )
+                adapter.drop_relation(new_relation)
+            elif adapter.type() == "snowflake":
+                create_stmt = (
+                    f"create or replace table {original_relation} clone {new_relation}"
+                )
+                _execute_sql(
+                    project_dir,
+                    profiles_dir,
+                    six.text_type(create_stmt).strip(),
+                    profile_target=profile_target,
+                    adapter=adapter,
+                )
+                adapter.drop_relation(new_relation)
+            else:
+                adapter.rename_relation(new_relation, original_relation)
 
-        config = parse.get_dbt_config(
-            project_dir=project_dir,
-            profiles_dir=profiles_dir,
-            profile_target=profile_target,
-        )
-        _clear_relations_cache(adapter, config)
+            adapter.commit_if_has_connection()
 
 
 def _drop_relation(
@@ -508,11 +544,21 @@ def _drop_relation(
     profile_target: str,
 ):
     adapter = _get_adapter(project_dir, profiles_dir, profile_target)
+    config = parse.get_dbt_config(
+        project_dir=project_dir,
+        profiles_dir=profiles_dir,
+        profile_target=profile_target,
+    )
 
     with adapter.connection_named(_connection_name("drop_relation", relation)):
-        adapter.connections.begin()
-        adapter.drop_relation(relation)
-        adapter.commit_if_has_connection()
+        with _cache_lock("_drop_relation"):
+            adapter.connections.begin()
+
+            _clear_relations_cache(adapter, config)
+
+            adapter.drop_relation(relation)
+
+            adapter.commit_if_has_connection()
 
 
 def _alchemy_engine(adapter: SQLAdapter, database: Optional[str]):
