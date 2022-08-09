dc8cf96923998a2caf34877d18dbfb3db2c07751
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-08-09 12:30:26 -0400

    feat: support singular tests

--- src/fal/__init__.py
+++ src/fal/__init__.py
@@ -1,4 +1,11 @@
 from dbt.contracts.results import NodeStatus
 
-from faldbt.project import FalDbt, DbtModel, DbtSource, DbtTest
+from faldbt.project import (
+    FalDbt,
+    DbtModel,
+    DbtSource,
+    DbtTest,
+    DbtGenericTest,
+    DbtSingularTest,
+)
 from fal.fal_script import Context, CurrentModel
