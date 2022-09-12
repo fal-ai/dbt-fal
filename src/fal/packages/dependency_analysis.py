41dea6be6fb08e988c1700080a499de23b3347c6
Batuhan Taskaya <isidentical@gmail.com>
2022-09-12 19:39:42 +0300

    fix: correct dependency analysis for external dbt packages

--- src/fal/packages/dependency_analysis.py
+++ src/fal/packages/dependency_analysis.py
@@ -1,45 +1,26 @@
 from __future__ import annotations
 
-import glob
-import importlib
-import importlib.util
-import os
+import importlib_metadata
 from pathlib import Path
 from typing import Iterator, Optional, Tuple
 
 
-# NOTE: This is from dbt https://github.com/dbt-labs/dbt-core/blob/7bd861a3514f70b64d5a6c642b4204b50d0d3f7e/core/dbt/version.py#L209-L236
-def _get_dbt_plugins_info() -> Iterator[Tuple[str, str]]:
-    for plugin_name in _get_adapter_plugin_names():
-        if plugin_name == "core":
-            continue
-        try:
-            mod = importlib.import_module(f"dbt.adapters.{plugin_name}.__version__")
-        except ImportError:
-            # not an adapter
-            continue
-        yield plugin_name, mod.version  # type: ignore
-
-
-def _get_adapter_plugin_names() -> Iterator[str]:
-    spec = importlib.util.find_spec("dbt.adapters")
-    # If None, then nothing provides an importable 'dbt.adapters', so we will
-    # not be reporting plugin versions today
-    if spec is None or spec.submodule_search_locations is None:
-        return
-
-    for adapters_path in spec.submodule_search_locations:
-        version_glob = os.path.join(adapters_path, "*", "__version__.py")
-        for version_path in glob.glob(version_glob):
-            # the path is like .../dbt/adapters/{plugin_name}/__version__.py
-            # except it could be \\ on windows!
-            plugin_root, _ = os.path.split(version_path)
-            _, plugin_name = os.path.split(plugin_root)
-            yield plugin_name
+def _get_dbt_packages() -> Iterator[Tuple[str, str]]:
+    # package_distributions will return a mapping of top-level package names to a list of distribution names (
+    # the PyPI names instead of the import names). An example distirbution info is the following, which
+    # contains both the main exporter of the top-level name (dbt-core) as well as all the packages that
+    # export anything to that namespace:
+    #   {"dbt": ["dbt-core", "dbt-postgres", "dbt-athena-adapter"]}
+    #
+    # This won't only include dbt.adapters.xxx, but anything that might export anything to the dbt namespace
+    # (e.g. a hypothetical plugin that only exports stuff to dbt.includes.xxx) which in theory would allow us
+    # to replicate the exact environment.
+    package_distributions = importlib_metadata.packages_distributions()
+    for dbt_plugin_name in package_distributions.get("dbt", []):
+        yield dbt_plugin_name, importlib_metadata.version(dbt_plugin_name)
 
 
 def get_default_requirements() -> Iterator[Tuple[str, Optional[str]]]:
-    import dbt.version
     import pkg_resources
     from dbt.semver import VersionSpecifier
 
@@ -56,7 +37,4 @@ def get_default_requirements() -> Iterator[Tuple[str, Optional[str]]]:
     else:
         yield "fal", raw_fal_version
 
-    yield "dbt-core", dbt.version.__version__
-
-    for adapter_name, adapter_version in _get_dbt_plugins_info():
-        yield f"dbt-{adapter_name}", adapter_version
+    yield from _get_dbt_packages()
