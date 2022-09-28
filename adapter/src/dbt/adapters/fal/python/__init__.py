85b67891e1c959c7dd3e3b4b8be88b00f916bdea
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-09-28 16:57:24 -0400

    Add fal dependency, move PythonAdapter class inside adaper (#556)
    * Move PythonAdapter base classes to adapter package
    
    * Change teleport to support
    
    * add fal dependency

---
+++ adapter/src/dbt/adapters/fal/python/__init__.py
@@ -0,0 +1,3 @@
+# these are all just exports, #noqa them so flake8 will be happy
+from dbt.adapters.fal.python.connections import PythonConnectionManager  # noqa
+from dbt.adapters.fal.python.impl import PythonAdapter  # noqa
