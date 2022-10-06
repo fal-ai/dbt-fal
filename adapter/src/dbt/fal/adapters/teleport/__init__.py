30dabf5036e5089135ae1c68973dc7985538f251
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-10-06 18:56:40 -0400

    Add generic Teleport details

---
+++ adapter/src/dbt/fal/adapters/teleport/__init__.py
@@ -0,0 +1,3 @@
+# these are all just exports, #noqa them so flake8 will be happy
+from .impl import TeleportAdapter  # noqa
+from .info import TeleportInfo, S3TeleportInfo, LocalTeleportInfo  # noqa
