c22f38e4fd57df8f7f8813cdffad98ae74ba6d24
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-10-06 18:53:34 -0400

    Move PythonAdapter class to another module

--- adapter/src/dbt/adapters/fal/python/__init__.py
+++
@@ -1,3 +0,0 @@
-# these are all just exports, #noqa them so flake8 will be happy
-from dbt.adapters.fal.python.connections import PythonConnectionManager  # noqa
-from dbt.adapters.fal.python.impl import PythonAdapter  # noqa
