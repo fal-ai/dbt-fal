1c5c2a645c0c4622f867cdf5e328a7ffdb0fd4eb
mederka <meder@kamalov.net>
2022-10-13 12:52:33 +0000

    Make teleport work with S3

--- adapter/src/dbt/adapters/fal/teleport.py
+++ adapter/src/dbt/adapters/fal/teleport.py
@@ -1,6 +1,7 @@
 from typing import Any, Callable, Dict, NewType
 from functools import partial
 import functools
+from dbt.adapters.fal.connections import TeleportTypeEnum
 import pandas as pd
 
 from dbt.contracts.connection import AdapterResponse
@@ -28,7 +29,8 @@ def _teleport_df_from_external_storage(teleport_info: TeleportInfo, locations: D
     if teleport_info.format == 'parquet':
         relation_path = locations[relation]
         url = teleport_info.build_url(relation_path)
-        return pd.read_parquet(url)
+        storage_options = _build_teleport_storage_options(teleport_info)
+        return pd.read_parquet(url, storage_options=storage_options)
     else:
         # TODO: support more
         raise RuntimeError(f"Format {teleport_info.format} not supported")
@@ -37,11 +39,26 @@ def _teleport_df_to_external_storage(teleport_info: TeleportInfo, locations: Dat
     if teleport_info.format == 'parquet':
         relation_path = teleport_info.build_relation_path(relation)
         url = teleport_info.build_url(relation_path)
-        data.to_parquet(url)
+        storage_options = _build_teleport_storage_options(teleport_info)
+
+        data.to_parquet(url, storage_options=storage_options)
         locations[relation] = relation_path
         return relation_path
     else:
         raise RuntimeError(f"Format {teleport_info.format} not supported")
+
+def _build_teleport_storage_options(teleport_info: TeleportInfo) -> Dict[str, str]:
+    storage_options = {}
+    if teleport_info.credentials.type == TeleportTypeEnum.REMOTE_S3:
+        storage_options = {
+            "key": teleport_info.credentials.s3_access_key_id,
+            "secret": teleport_info.credentials.s3_access_key
+        }
+    elif teleport_info.credentials.type == TeleportTypeEnum.LOCAL:
+        pass
+    else:
+        raise RuntimeError(f"Teleport storage type {teleport_info.credentials.type} not supported")
+    return storage_options
 
 def run_with_teleport(code: str, teleport_info: TeleportInfo, locations: DataLocation) -> str:
     # main symbol is defined during dbt-fal's compilation
