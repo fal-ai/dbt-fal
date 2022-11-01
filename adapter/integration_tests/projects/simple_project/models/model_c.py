ae70f7165cae8400ec435cbceb295468ae87d847
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-11-01 10:49:39 -0400

    update tests to new materialized

--- adapter/integration_tests/projects/simple_project/models/model_c.py
+++ adapter/integration_tests/projects/simple_project/models/model_c.py
@@ -2,7 +2,7 @@ from _fal_testing.utils import create_model_artifact
 from fal.typing import *
 
 def model(dbt, fal):
-    dbt.config(materialized="fal_table")
+    dbt.config(materialized="table")
     df = dbt.ref("model_b")
 
     df["my_bool"] = True
