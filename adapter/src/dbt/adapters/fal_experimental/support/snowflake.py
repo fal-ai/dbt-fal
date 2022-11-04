93a2a606bed9db1ab6b00e4f733043719650444e
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-11-03 20:04:18 -0400

    typing hints

--- adapter/src/dbt/adapters/fal_experimental/support/snowflake.py
+++ adapter/src/dbt/adapters/fal_experimental/support/snowflake.py
@@ -1,17 +1,25 @@
-from dbt.adapters.base import BaseAdapter, BaseRelation
+from dbt.adapters.base import BaseRelation
 from dbt.adapters.base.connections import AdapterResponse
 from dbt.adapters.fal_experimental.adapter_support import new_connection
 import pandas as pd
-from dbt.adapters.sql import SQLAdapter
+
+# [snowflake] extras dependencies
+import snowflake.connector as snowflake
+
+from dbt.adapters.snowflake import SnowflakeAdapter, SnowflakeConnectionManager
 
 
-def read_relation_as_df(adapter: BaseAdapter, relation: BaseRelation) -> pd.DataFrame:
+def read_relation_as_df(
+    adapter: SnowflakeAdapter, relation: BaseRelation
+) -> pd.DataFrame:
     sql = f"SELECT * FROM {relation}"
 
     assert adapter.type() == "snowflake"
 
     with new_connection(adapter, "fal-snowflake:read_relation_as_df") as conn:
-        cur = conn.handle.cursor()
+        handle: snowflake.SnowflakeConnection = conn.handle
+        cur = handle.cursor()
+
         cur.execute(sql)
         df: pd.DataFrame = cur.fetch_pandas_all()
 
@@ -28,28 +36,25 @@ def read_relation_as_df(adapter: BaseAdapter, relation: BaseRelation) -> pd.Data
 
 
 def write_df_to_relation(
-    adapter: SQLAdapter,
+    adapter: SnowflakeAdapter,
     data: pd.DataFrame,
     relation: BaseRelation,
 ) -> AdapterResponse:
-    from dbt.adapters.snowflake import SnowflakeAdapter, SnowflakeConnectionManager
-    import snowflake.connector as snowflake
     import snowflake.connector.pandas_tools as snowflake_pandas
 
     assert adapter.type() == "snowflake"
-
-    _adapter: SnowflakeAdapter = adapter  # type: ignore
 
     database: str = relation.database  # type: ignore
     schema: str = relation.schema  # type: ignore
     table: str = relation.identifier  # type: ignore
 
     with new_connection(adapter, "fal-snowflake:write_df_to_relation") as conn:
-        connection_manager: SnowflakeConnectionManager = _adapter.connections  # type: ignore
+        connection_manager: SnowflakeConnectionManager = adapter.connections  # type: ignore
+        handle: snowflake.SnowflakeConnection = conn.handle
 
         with connection_manager.exception_handler("LOAD TABLE"):
             success, _, num_rows, output = snowflake_pandas.write_pandas(
-                conn.handle,
+                handle,
                 data,
                 table_name=table,
                 database=database,
