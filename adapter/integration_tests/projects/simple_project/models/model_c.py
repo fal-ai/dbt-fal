b03d193d16b74d894b18378456746a382a52baad
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-11-08 23:55:08 -0400

    add local import to integration tests

--- adapter/integration_tests/projects/simple_project/models/model_c.py
+++ adapter/integration_tests/projects/simple_project/models/model_c.py
@@ -1,6 +1,8 @@
 def model(dbt, fal):
+    from get_bool import get_bool
+
     dbt.config(materialized="table")
     df = dbt.ref("model_b")
 
-    df["my_bool"] = True
+    df["my_bool"] = get_bool()
     return df
