e88f1413d761631b4d5fe3f736f0a8fdbb2926ca
mederka <meder@kamalov.net>
2022-12-22 11:43:12 +0000

    Add source freshness project

---
+++ adapter/integration_tests/projects/source_freshness/load_freshness_table.py
@@ -0,0 +1,22 @@
+from datetime import datetime as dt, timezone as tz
+from pandas import DataFrame
+from fal import FalDbt
+from sys import argv
+
+project_dir = argv[1] if len(argv) >= 2 else "."
+profiles_dir = argv[2] if len(argv) >= 3 else ".."
+
+faldbt = FalDbt(project_dir=project_dir, profiles_dir=profiles_dir)
+
+# 10 rows
+df = DataFrame({"loaded_at": dt.now(tz=tz.utc).isoformat(), "info": range(0, 10)})
+
+print(df)
+faldbt.write_to_source(df, "freshness_test", "freshness_table", mode="overwrite")
+
+from time import sleep
+
+# Let BigQuery cache load it
+sleep(5)
+
+print("Loaded")
---
+++ adapter/integration_tests/projects/source_freshness/models/model_a.py
@@ -0,0 +1,8 @@
+def model(dbt, fal):
+    dbt.config(materialized="table")
+    dbt.config(fal_environment="funny")
+    import pyjokes
+    joke = pyjokes.get_joke()
+    df = dbt.source("freshness_test", "freshness_table")
+    df["my_joke"] = joke
+    return df
