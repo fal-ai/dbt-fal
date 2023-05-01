6e6771fe1ef88934e41029e9c16bee18cfb1442b
Matteo Ferrando <matteo.ferrando2@gmail.com>
2023-05-01 18:36:46 -0400

    re-add temp_dir to environment variables

--- projects/fal/integration_tests/features/steps/fal_steps.py
+++ projects/fal/integration_tests/features/steps/fal_steps.py
@@ -66,6 +66,7 @@ def set_project_folder(context, project: str):
     context.base_dir = str(project_path)
     context.temp_dir = tempfile.TemporaryDirectory()
     os.environ["project_dir"] = context.base_dir
+    os.environ["temp_dir"] = context.temp_dir.name
     os.environ["DBT_TARGET_PATH"] = target_path(context)
 
 
