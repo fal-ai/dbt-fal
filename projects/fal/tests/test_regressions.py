09a99811d4dbe42709531792d3448e2584823465
Matteo Ferrando <matteo.ferrando2@gmail.com>
2023-05-02 18:24:05 -0400

    use target-path in dbt_project.yml while dbt issue is fixed

--- projects/fal/tests/test_regressions.py
+++ projects/fal/tests/test_regressions.py
@@ -18,6 +18,6 @@ def test_write_to_source_not_processing_jinja():
 
     faldbt.write_to_source(df, "test_sources", "sql_col")
 
-    # TODO: look at df data
+    # The sql string should not be processed by jinja and written as is
     df = faldbt.source("test_sources", "sql_col")
     assert df.sql.get(0) == r"SELECT 1 FROM {{ wrong jinja }}"
