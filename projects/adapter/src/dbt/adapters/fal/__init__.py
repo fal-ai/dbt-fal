a943e3587661d86f8527138dba2fc0d6378f3c8a
Matteo Ferrando <matteo.ferrando2@gmail.com>
2023-05-16 16:26:50 -0400

    fix(dbt-fal): handle dbt debug and dbt --version commands

--- projects/adapter/src/dbt/adapters/fal/__init__.py
+++ projects/adapter/src/dbt/adapters/fal/__init__.py
@@ -1,11 +1,21 @@
 from dbt.adapters.base import AdapterPlugin
 
 from dbt.adapters.fal.connections import FalEncCredentials
-from dbt.adapters.fal.impl import FalEncAdapter
 from dbt.include import fal
 
-Plugin = AdapterPlugin(
-    adapter=FalEncAdapter,
-    credentials=FalEncCredentials,
-    include_path=fal.PACKAGE_PATH
-)
+# Avoid loading the plugin code for any import
+def __getattr__(name):
+    if name == "FalEncAdapter":
+        from dbt.adapters.fal.impl import FalEncAdapter
+
+        return FalEncAdapter
+    if name == "Plugin":
+        from dbt.adapters.fal.impl import FalEncAdapter
+
+        return AdapterPlugin(
+            adapter=FalEncAdapter,
+            credentials=FalEncCredentials,
+            include_path=fal.PACKAGE_PATH
+        )
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
