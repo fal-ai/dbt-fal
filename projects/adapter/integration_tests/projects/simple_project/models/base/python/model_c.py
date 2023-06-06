6f83e1a6c24eb83881b43c3af8b6e9446ea83dd8
Matteo Ferrando <matteo.ferrando2@gmail.com>
2023-06-06 12:08:04 -0400

    tests: modify adapter tests for post-hooks

--- projects/adapter/integration_tests/projects/simple_project/models/base/python/model_c.py
+++ projects/adapter/integration_tests/projects/simple_project/models/base/python/model_c.py
@@ -1,7 +1,11 @@
 def model(dbt, fal):
     from utils.get_bool import get_bool
+    import json
+
+    dbt.config(materialized="table")
 
     df = dbt.ref("model_b")
 
     df["my_bool"] = get_bool()
+    df["my_json"] = json.dumps({"a": 1, "b": 2})
     return df
